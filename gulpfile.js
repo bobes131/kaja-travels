e3f535b075c68a420c792902d8073df0e2cfc933
Filip <bobes131@gmail.com>
2017-09-19 14:05:32 +0200

    pocatek tvory stranky

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require("gulp");
 var watch = require("gulp-watch");
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssimport = require('postcss-import');
 
 gulp.task("default", function() {
    console.log('Default'); 
@@ -10,10 +15,15 @@ gulp.task("html", function() {
 });
 
 gulp.task("styles", function() {
-    console.log('CSS task');
+   return gulp.src('./app/assets/styles/styles.css')
+       .pipe(postcss([cssimport,cssvars, nested, autoprefixer]))
+       .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task("watch", function() {
+    
+    
+    
     watch("./app/index.html", function() {
         gulp.start('html');
     });
