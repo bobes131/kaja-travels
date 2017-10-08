95d37747cdf42d07058bea8a268bfa17553f0102
Filip <bobes131@gmail.com>
2017-10-08 23:14:05 +0200

    První lekce javascriptu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,13 @@
-console.log('This is a test');
+function Person(fullName,favColour) {
+   this.name = fullName;
+   this.favouriteColor = favColour;
+   this.greet = function() {
+       console.log('Hello my name is'+this.name+" and my fav colour is "+this.favouriteColor+".");
+   } 
+}
+
+var john = new Person("John",'zelena');
+john.greet();
+
+var john = new Person("Kurva",'Hovna');
+john.greet();
