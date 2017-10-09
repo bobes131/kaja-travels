233e8f829bb8110b877455305fbc12df59fe56b7
Filip <bobes131@gmail.com>
2017-10-09 12:08:17 +0200

    Došel sem až do Babel ale to je na mě už ted moc

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,12 @@
-function Person(fullName,favColour) {
-   this.name = fullName;
-   this.favouriteColor = favColour;
-   this.greet = function() {
-       console.log('Hello my name is'+this.name+" and my fav colour is "+this.favouriteColor+".");
-   } 
-}
-
+var Person = require('./modules/Person');
+var $ = require('jquery');
+console.log(Person);
+   alert("Fuck"); 
 var john = new Person("John",'zelena');
 john.greet();
 
 var john = new Person("Kurva",'Hovna');
 john.greet();
+
+
+$("h1").remove();
