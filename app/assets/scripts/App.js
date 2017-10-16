4cc379cc71c2f140d162944b22a22697c98d4785
Filip <bobes131@gmail.com>
2017-10-16 14:21:22 +0200

    Dokončení modalu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
@@ -9,3 +10,4 @@ var mobileMenu = new MobileMenu();
  new RevealOnScroll($('.testimonial'),"60%"); 
 
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
